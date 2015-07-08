3f816f9bbbbd15e66168cae41a02d533b8b588ab
amandarich <amandarich823@gmail.com>
2015-07-08 16:38:18 -0400

    new location

---
+++ inc/footer.php
@@ -0,0 +1,60 @@
+
+		<footer>
+			<div class="footerWrapper">
+
+				<div class="footerContact">
+					<p><span>CONQUEST BREWERY</span></br>947 S Stadium Road, Bay 1</br>Columbia, SC 29201</p>
+					<p>General Inquiries: 
+					<a href="mailto:info@conquestbrewing.com?Subject=Hello%20again" target="_top">info@conquestbrewing.com</a></p>
+					<p>Event Planning/Marketing Inquiries:
+					<a href="mailto:marketingatconquest@gmail.com?Subject=Hello%20again" target="_top"> marketingatconquest@gmail.com</a></p>
+				</div>
+
+				<ul class="clearfix iconBox">
+					<li class="icons fb"><img src="img/fbIcon.png" alt="FaceBook Icon"></li>
+					<li class="icons"><img src="img/twitterIcon.png" alt="Twitter"></li>
+				</ul>
+
+				<div class="footerNewsletter">
+					<a class="input" href="#" class="btn btn-info" role="button">SIGN UP FOR OUR EMAIL NEWLETTER</a>
+				</div>
+
+				
+
+			</div><!--End .footerWrapper div-->
+		</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/script.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+         <script>
+        //     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        //     function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        //     e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        //     e.src='https://www.google-analytics.com/analytics.js';
+        //     r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        //     ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        // </script>
+    </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/jquery.js"></script>
+    </head>
+    <body>
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+		
+
+
+		<header>
+			<div class="headerWrapper">
+
+				<div class="headerLogo">
+					<a href="index.html">
+						<img src="img/conquest-logo.png">
+					</a>
+				</div>
+				<nav class="main_navigation">
+					<a class="button" href="#">
+						<h5>Menu</h5>
+					</a>
+						<ul class="clearfix main_nav">
+							<li><a href="index.html">HOME</a></li>
+							<li><a href="tastingroom.html">TASTING ROOM</a></li>
+							<li><a href="ourbeers.html">CONQUEST BEERS</a></li>
+							<li><a href="aboutus.html">ABOUT CONQUEST</a></li>
+						</ul>
+				</nav>
+
+			</div>    <!--End .headerWrapper div-->
+		</header>
